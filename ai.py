399c618ef79a9dafba3f2a9dc1ad2333531c73bb
kvasovanatolii-svg <kvasovanatolii@gmail.com>
2026-03-05 21:40:06 +0500

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,1 +1,24 @@
+from openai import OpenAI
+import os
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def ai_answer(text):
+
+    try:
+
+        r = client.chat.completions.create(
+
+            model="gpt-4o-mini",
+
+            messages=[
+                {"role":"system","content":"Ты справочный бот клиники"},
+                {"role":"user","content":text}
+            ]
+        )
+
+        return r.choices[0].message.content
+
+    except:
+
+        return ""
