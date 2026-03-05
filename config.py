da7ef5b71466a9f784f0d7f4d5809b5071fa147c
kvasovanatolii-svg <kvasovanatolii@gmail.com>
2026-03-05 21:34:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
 
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SPREADSHEET_ID = os.getenv("GOOGLE_SPREADSHEET_ID")
+SERVICE_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+SHEET_DOCTORS = "Врачи"
+SHEET_PRICES = "Цены"
+SHEET_PREP = "Подготовка"
+SHEET_SCHEDULE = "Расписание"
+SHEET_REQUESTS = "Записи"
+SHEET_INFO = "Инфо"
